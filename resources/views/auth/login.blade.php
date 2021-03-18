6c3f3baac17fb8c1435e1aaac54cf5589cd64dd2
Xavier K <xavierkhew00@gmail.com>
2021-03-19 07:45:01 +0800

    add

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-4/12 bg-white p-6 rounded">
-            Register
+            Login
             <br><br>
 
             @if (session('status'))
