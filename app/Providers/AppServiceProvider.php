d63748289fcf149792287ce7c2ae7c10d73f2fc5
Xavier K <xavierkhew00@gmail.com>
2021-03-20 22:54:59 +0800

    add https try

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,27 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Routing\UrlGenerator;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+     /**
+      * Bootstrap any application services.
+      *
+      * @return void
+      */
+      public function boot(UrlGenerator $url)
+      {
+              if (env('APP_ENV') !== 'local') {
+                    $url->forceScheme('https');
+              }
+       }
+      /**
+       * Register any application services.
+       *
+       * @return void
+       */
+       public function register()
+       {
+              //
+       }
 }
